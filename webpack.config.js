5ab07624b4540c500b4757672c5a0625bb670876
Franco Lepe <Francolepe169@gmail.com>
2024-09-02 14:27:23 -0400

    changed from dev mode to production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,73 @@
+// const path = require('path');
+// const Dotenv = require('dotenv-webpack');
+
+// module.exports = {
+//   mode: 'development',
+//   entry: './src/script.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//     publicPath: '/',
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/i,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//     ],
+//   },
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, 'dist'),
+//     },
+//     compress: true,
+//     port: 9000,
+//   },
+//   plugins: [
+//     new Dotenv(),
+//   ],
+// };
+
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/script.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/script.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
     },
-    compress: true,
-    port: 9000,
-  },
-  plugins: [
-    new Dotenv(),
-  ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      compress: true,
+      port: 9000,
+    },
+    plugins: [
+      new Dotenv(),
+    ],
+    optimization: isProduction ? {
+      // Example optimization for production
+      splitChunks: {
+        chunks: 'all',
+      },
+    } : {},
+  };
 };
 
 
